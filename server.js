6af04e3cbed7ecaddbb675eb1a8192f01d5074c2
Apple <apple@Apples-MacBook-Pro.local>
2025-07-10 17:44:18 +0530

    button in index.ejs

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine','ejs')
+
 app.get('/', (req,res) => {
-    
+    res.render('index')
 })
 
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3001);
+
